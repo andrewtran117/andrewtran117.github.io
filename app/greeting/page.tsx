72f10760478f9aa560c1555684445dc758636d4b
adrewtran117 <adrewtran117@gmail.com>
2025-01-11 13:55:24 -0500

    mobile reorganize

--- app/greeting/page.tsx
+++ app/greeting/page.tsx
@@ -2,14 +2,14 @@
 
 export default function Greetings(){
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 w-full max-w-6xl px-4 animate-fadeInSmooth transform" style={{ transform: 'translateX(8%)' }}>
+        <div className="grid grid-cols-1 md:grid-cols-2 w-full max-w-6xl px-4 animate-fadeInSmooth transform">
             <div className="flex flex-col justify-center pb-8">
                 <h1 className="text-4xl sm:text-5xl md:text-6xl mb-4 text-[#44624A]">Hi, I&apos;m Andrew</h1>
                 <p className="text-lg sm:text-xl md:text-2xl mb-2 text-[#44624A]">NYC based Software Engineer</p>
                 <p className="text-base sm:text-lg md:text-xl text-gray-600 pt-12">Currently</p>
                 <p className="text-lg sm:text-xl md:text-2xl text-[#44624A]">Software Development Engineer for AWS</p>
             </div>
-            <div className="flex items-center justify-center">
+            <div className="flex items-center justify-center" style={{ transform: 'translateX(8%)' }}>
                 <img
                     src="/matcha2.png"
                     alt="Matcha Glass"
