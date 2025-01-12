41d8f4a73e464cd78ba4ab83e56a4e9f15fdf187
adrewtran117 <adrewtran117@gmail.com>
2025-01-11 23:21:47 -0500

    change vibe

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -5,25 +5,25 @@ import { motion } from "framer-motion";
 
 const HeroSection = () => {
   return (
-    <section className="lg:py-16">
+    <section className="py-8">  {/* Reduced from lg:py-16 */}
       <div className="grid grid-cols-1 sm:grid-cols-12">
         <motion.div
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.5 }}
+          transition={{ duration: 1 }}
           className="col-span-8 place-self-center justify-self-center text-center sm:justify-self-start sm:text-left text-[#44624a]"
         >
           <h1 className="mb-4 text-2xl sm:text-3xl lg:text-6xl lg:leading-normal font-extrabold">
             Hi, I&apos;m Andrew Tran
           </h1>
-          <p className="text-base sm:text-lg mb-6 lg:text-xl pb-10">
+          <p className="text-base sm:text-lg mb-6 lg:text-xl pb-6"> {/* Reduced padding-bottom */}
             Software Engineer based in NYC
           </p>
         </motion.div>
         <motion.div
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.5 }}
+          transition={{ duration: 1 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0 flex justify-center"
         >
           <div className="w-[200px] h-[200px] sm:w-[250px] sm:h-[250px] lg:w-[400px] lg:h-[400px] relative">
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,34 @@
-import Navbar from "./components/Navbar";
-import HeroSection from "./components/HeroSection";
+// import Navbar from "./components/Navbar";
+// import HeroSection from "./components/HeroSection";
+// import AboutSection from "./components/AboutSection";
+
+// export default function Home() {
+
+//   return (
+//     <main className="flex min-h-screen flex-col bg-[#f1ebe1]">
+//       <Navbar />
+//       <div className="container mt-8 mx-auto px-12 py-4">
+//         <div id="hero">
+//           <HeroSection/>
+//         </div>
+//         {/* <div id="about">
+//           <AboutSection/>
+//         </div> */}
+//       </div>
+//     </main>
+//   );
+// }
+
+
+import ProfileGrid from "./components/GridSection";
 
 export default function Home() {
-
   return (
     <main className="flex min-h-screen flex-col bg-[#f1ebe1]">
-      <Navbar />
-      <div className="container mt-24 mx-auto px-12 py-4">
-        <HeroSection/>
+      <div className="pt-[0.25in] pl-[.5in] pr-[.5in]">
+        <ProfileGrid />
       </div>
     </main>
   );
 }
+
