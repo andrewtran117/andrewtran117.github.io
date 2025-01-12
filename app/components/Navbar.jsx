35d508464de992e0a6010be04b40174ed6042b5b
adrewtran117 <adrewtran117@gmail.com>
2025-01-11 21:31:22 -0500

    center everything and good tab bar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
 
   return (
-    <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
+    <nav className="fixed mx-auto top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
       <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
         <Link
           href={"/"}
@@ -35,16 +35,16 @@ const Navbar = () => {
           {!navbarOpen ? (
             <button
               onClick={() => setNavbarOpen(true)}
-              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
+              className="flex items-center px-3 py-2 rounded text-slate-200 hover:text-white"
             >
-              3 bars
+              Bars
             </button>
           ) : (
             <button
               onClick={() => setNavbarOpen(false)}
-              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
+              className="flex items-center px-3 py-2 rounded text-slate-200 hover:text-white"
             >
-              XXX
+              X
             </button>
           )}
         </div>
