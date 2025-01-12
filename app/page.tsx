ed2dd99964bdcca1f0a00ee4782580e359052288
adrewtran117 <adrewtran117@gmail.com>
2025-01-11 23:24:45 -0500

    change vibe

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,11 @@
-// import Navbar from "./components/Navbar";
-// import HeroSection from "./components/HeroSection";
-// import AboutSection from "./components/AboutSection";
-
-// export default function Home() {
-
-//   return (
-//     <main className="flex min-h-screen flex-col bg-[#f1ebe1]">
-//       <Navbar />
-//       <div className="container mt-8 mx-auto px-12 py-4">
-//         <div id="hero">
-//           <HeroSection/>
-//         </div>
-//         {/* <div id="about">
-//           <AboutSection/>
-//         </div> */}
-//       </div>
-//     </main>
-//   );
-// }
-
-
 import ProfileGrid from "./components/GridSection";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#f1ebe1]">
-      <div className="pt-[0.25in] pl-[.5in] pr-[.5in]">
+      <div className="pt-[0.25in] sm:pl-[0.5in] sm:pr-[0.5in] md:pl-[1in] md:pr-[1in] lg:pl-[2in] lg:pr-[2in]">
         <ProfileGrid />
       </div>
     </main>
   );
 }
-
