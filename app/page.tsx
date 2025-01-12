c91a0b456f953895e1fae38f035e85d9e1d02195
adrewtran117 <adrewtran117@gmail.com>
2025-01-11 23:26:12 -0500

    change vibe

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import ProfileGrid from "./components/GridSection";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#f1ebe1]">
-      <div className="pt-[0.25in] sm:pl-[0.5in] sm:pr-[0.5in] md:pl-[1in] md:pr-[1in] lg:pl-[2in] lg:pr-[2in]">
+      <div className="pt-[0.25in] sm:pl-[0.25in] sm:pr-[0.25in] md:pl-[.25in] md:pr-[.25in] lg:pl-[.25in] lg:pr-[.25in]">
         <ProfileGrid />
       </div>
     </main>
