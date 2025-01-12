629c86517d4b0a726c9a27ea5262b774c27f50bc
adrewtran117 <adrewtran117@gmail.com>
2025-01-12 00:08:29 -0500

    change vibe

--- app/components/GridSection.tsx
+++ app/components/GridSection.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ProfileGrid: React.FC = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-[1fr_3fr] gap-y-8 sm:gap-x-16 p-[1in] text-[#4B7F52] font-sans">
+    <div className="grid grid-cols-1 sm:grid-cols-[1fr_3fr] gap-y-8 sm:gap-x-16 p-[1in] text-[#f9f9f9] font-sans">
       {/* 1st Row - 1st Column */}
       <div className="flex flex-col">
         <div className="text-2xl font-bold">Andrew Tran</div>
@@ -13,22 +13,26 @@ const ProfileGrid: React.FC = () => {
       {/* 1st Row - 2nd Column */}
       <div>
         <p className="text-base leading-relaxed">
-          I am a passionate software engineer with a deep interest in AI, cloud computing, and customer-centric
-          solutions. I enjoy working on projects that enhance user experiences and aim to make impactful contributions to
-          the tech industry.
+        I am driven by the constant desire to learn, grow, and
+        push the boundaries of what’s possible.
+        My work has improved customer experiences, enhanced urban safety, and made technology more accessible
+        in communities, all while driving innovation in impactful ways.
+        I attribute my success to the people around me, mentors, colleagues, and communities, who
+        continuously inspire and empower me to elevate my work and make a lasting difference.
+
         </p>
       </div>
 
       {/* 2nd Row - 1st Column */}
       <div>
-        <a href="mailto:andrew.tran117@outlook.com" className="text-[#4B7F52] hover:text-[#3A5A3F] underline">
+        <a href="mailto:andrew.tran117@outlook.com" className="text-[#f9f9f9] underline">
           Contact
         </a>
       </div>
 
       {/* 2nd Row - 2nd Column */}
       <div>
-        <a href="https://drive.google.com/file/d/1imDv7-Oz0AB6ISDQGXQdzRUD2LvwHUW2/view?usp=sharing" className="text-[#4B7F52] hover:text-[#3A5A3F] underline" target="_blank" rel="noopener noreferrer">
+        <a href="https://drive.google.com/file/d/1imDv7-Oz0AB6ISDQGXQdzRUD2LvwHUW2/view?usp=sharing" className="text-[#f9f9f9] underline" target="_blank" rel="noopener noreferrer">
           Resume
         </a>
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import ProfileGrid from "./components/GridSection";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#f1ebe1]">
+    <main className="flex min-h-screen flex-col bg-[#202020]">
       <div className="pt-[0.25in] sm:pl-[0.25in] sm:pr-[0.25in] md:pl-[.25in] md:pr-[.25in] lg:pl-[.25in] lg:pr-[.25in]">
         <ProfileGrid />
       </div>
