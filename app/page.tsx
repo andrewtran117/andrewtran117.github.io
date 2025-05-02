4fb327cee0f1f7aed5af576837c09de4cb2977e4
adrewtran117 <adrewtran117@gmail.com>
2025-05-02 19:52:28 -0400

    &apos;

--- app/page.tsx
+++ app/page.tsx
@@ -18,10 +18,10 @@ export default function Home() {
         <div className="mb-12 pr-16 pl-0">
           <h2 className="text-2xl font-bold mb-4">About Me</h2>
             <p className="text-base leading-relaxed">
-              I'm an engineer joining AWS this summer in New York, NY. I have previously held internships at AWS, the Cigna Group, IBM, and AeroPest (autonomous AI drone startup for pest control).
+              I&apos;m an engineer joining AWS this summer in New York, NY. I have previously held internships at AWS, the Cigna Group, IBM, and AeroPest (autonomous AI drone startup for pest control).
             </p>
             <p className="text-base leading-relaxed mt-6">
-              Beyond engineering, I am interested in human-computer interaction and Generative AI safety, working to ensure that intelligent systems align with human needs and values. I have conducted this work during my undergraduate studies at Temple University under the supervision of&nbsp;
+              Beyond engineering, I&apos;m interested in human-computer interaction and Generative AI safety, working to ensure that intelligent systems align with human needs and values. I have conducted this work during my undergraduate studies at Temple University under the supervision of&nbsp;
               <a 
                 href="https://scholar.google.com/citations?user=leLEF3wAAAAJ&hl=en" 
                 className="text-[#d32f2f] underline hover:text-[#b71c1c]" 
