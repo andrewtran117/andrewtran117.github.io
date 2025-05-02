0e8de43edbb01c35b55517be298b6edb55c558cd
adrewtran117 <adrewtran117@gmail.com>
2025-05-02 19:50:28 -0400

    new text

--- app/page.tsx
+++ app/page.tsx
@@ -17,14 +17,20 @@ export default function Home() {
         {/* Bio Section */}
         <div className="mb-12 pr-16 pl-0">
           <h2 className="text-2xl font-bold mb-4">About Me</h2>
-          <p className="text-base leading-relaxed">
-            I am driven by the constant desire to learn, grow, and
-            push the boundaries of what&apos;s possible.
-            My work has improved customer experiences, enhanced urban safety, and made technology more accessible
-            in communities, all while driving innovation in impactful ways.
-            I attribute my success to the people around me, mentors, colleagues, and communities, who
-            continuously inspire and empower me to elevate my work and make a lasting difference. I conducted research under the supervision of <a href="https://scholar.google.com/citations?user=leLEF3wAAAAJ&hl=en" className="text-[#d32f2f] underline hover:text-[#b71c1c]" target="_blank" rel="noopener noreferrer">Dr. Stephen MacNeil</a>.
-          </p>
+            <p className="text-base leading-relaxed">
+              I'm an engineer joining AWS this summer in New York, NY. I have previously held internships at AWS, the Cigna Group, IBM, and AeroPest (autonomous AI drone startup for pest control).
+            </p>
+            <p className="text-base leading-relaxed mt-6">
+              Beyond engineering, I am interested in human-computer interaction and Generative AI safety, working to ensure that intelligent systems align with human needs and values. I have conducted this work during my undergraduate studies at Temple University under the supervision of&nbsp;
+              <a 
+                href="https://scholar.google.com/citations?user=leLEF3wAAAAJ&hl=en" 
+                className="text-[#d32f2f] underline hover:text-[#b71c1c]" 
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                Dr. Stephen MacNeil
+              </a>.
+            </p>
         </div>
         
         {/* Research Experience Section */}
